a949a3125c9539f7b920e2683271f22ecc9f8ba2
Gianfranco Giulioni <g.giulioni@gmail.com>
2018-04-06 08:41:12 +0200

    source code

--- src/gabriele/institutions/LaborMarket.java
+++ src/gabriele/institutions/LaborMarket.java
@@ -1,3 +1,18 @@
+/*
+GABRIELE: the General Agent Based Repast Implemented Extensible Laboratory for Economics
+Copyright (C) 2018  Gianfranco Giulioni
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package gabriele.institutions;
 
 import repast.simphony.random.RandomHelper;
